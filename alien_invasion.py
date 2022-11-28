7dcd6871ebf8d3a384f5fb037c24aa3e7798ee70
george <gj158@qq.com>
2022-11-28 17:54:01 +0800

    finished chapter_12

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,8 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """管理游戏资源和行为的类"""
@@ -9,28 +11,90 @@ class AlienInvasion:
     def __init__(self):
         """初始化游戏并创建游戏资源。"""
         pygame.init()
+
         self.settings = Settings()
 
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
+        #默认在全屏模式下进行游戏。
+        # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
         #设置背景色
         self.bg_color = (230, 230, 230)
 
+        self.ship = Ship(self)
+
+        #创建子弹编组
+        self.bullets = pygame.sprite.Group()
+
     def run_game(self):
         """开始游戏的主循环。"""
         while True:
-            #监视键盘和鼠标事件。
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self.ship.update()
+            self._update_bullets()  
+            self._upadate_screen()
+            
+    def _check_events(self):
+        """响应按键和鼠标事件。（辅助方法）"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
 
-            #每次循环时都重绘屏幕。
-            self.screen.fill(self.settings.bg_color)
+    def _check_keydown_events(self, event):
+        """响应按键。"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
-            #让最近绘制的屏幕可见。
-            pygame.display.flip()
+    def _check_keyup_events(self, event):
+        """响应松开。"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _fire_bullet(self):
+        """创建一颗子弹，并将其加入至bullets编组中"""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """更新子弹的位置并删除消失的子弹"""
+        self.bullets.update()
+
+        #删除消失的子弹
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
+    def _upadate_screen(self):
+        """更新屏幕上的图像，并切换到新屏幕。（辅助方法）"""
+        #绘制背景。
+        self.screen.fill(self.settings.bg_color)
+
+        #绘制飞船。
+        self.ship.blitme()
+
+        #绘制子弹
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+
+        #让最近绘制的屏幕可见。
+        pygame.display.flip()
 
 if __name__ == '__main__':
     #创建游戏实例并运行游戏。
